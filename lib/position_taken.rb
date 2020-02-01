d340fd54edf71fecb5c0777f1aad5f9be41d7cfe
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-01 20:07:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,15 +1,13 @@
 # code your #position_taken? method here!
 def position_taken()
   puts "hello there"
-  # if board.size <= i
-  #   return false
-  # end
-  #
-  # if board[i] == 'X' || board[i] == 'O'
-  #   return true
-  # else
-  #   return false
-  # end
+  if board.size <= i
+    return false
+  end
+  
+  if board[i] == 'X' || board[i] == 'O'
+    return true
+  else
+    return false
+  end
 end
-
-position_taken
